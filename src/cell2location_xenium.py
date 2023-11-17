c70b290c371cecf3bcdf1e6ca821e56ccddb87f9
LucasBrunschwig <lucas.brunschwig@gmail.com>
2023-11-17 12:37:38 +0100

    added argument

--- src/cell2location_xenium.py
+++ src/cell2location_xenium.py
@@ -219,7 +219,7 @@ def signature_ref(annotated_ref_seq, label):
                                                        # cell type, co-variate used for constructing signatures
                                                        labels_key=label,
                                                        # multiplicative technical effects (platform, 3' - 5', donor)
-                                                       # categorical_covariate_keys=["DonorID"]
+                                                       categorical_covariate_keys=["age", "ChipID"]
                                                        )
 
     from cell2location.models import RegressionModel
@@ -279,7 +279,70 @@ def signature_ref(annotated_ref_seq, label):
     return adata_ref, inf_aver
 
 
-def cell2location_xenium(use_gene_intersection: bool = False, cell_types_granularity="taxonomy"):
+def run_cell2location(annotated_data, inf_aver):
+
+    sc.settings.set_figure_params(dpi=100, color_map='viridis', dpi_save=100,
+                                  vector_friendly=True,
+                                  facecolor='white')
+
+    # find shared genes and subset both anndata and reference signatures
+    intersect = np.intersect1d(annotated_data.var_names, inf_aver.index)
+    adata_vis = annotated_data[:, intersect].copy()
+    inf_aver = inf_aver.loc[intersect, :].copy()
+
+    # prepare anndata for cell2location model
+    cell2location.models.Cell2location.setup_anndata(adata=adata_vis, batch_key="sample")
+
+    # create and train the model
+    mod = cell2location.models.Cell2location(
+        adata_vis,
+        cell_state_df=inf_aver,
+        # the expected average cell abundance: tissue-dependent
+        # hyper-prior which can be estimated from paired histology:
+        N_cells_per_location=1,
+        # hyperparameter controlling normalisation of
+        # within-experiment variation in RNA detection:
+        detection_alpha=20
+    )
+    mod.view_anndata_setup()
+
+    mod.train(max_epochs=30000,
+              # train using full data (batch_size=None)
+              batch_size=None,
+              # use all data points in training because
+              # we need to estimate cell abundance at all locations
+              train_size=1,
+              use_gpu=True,
+              )
+
+    # plot ELBO loss history during training, removing first 100 epochs from the plot
+    mod.plot_history(1000)
+    plt.legend(labels=['full data training'])
+    plt.savefig(RESULTS_DIR / "plot_history_c2l.png")
+    plt.close()
+
+    # In this section, we export the estimated cell abundance (summary of the posterior distribution).
+    adata_vis = mod.export_posterior(
+        adata_vis, sample_kwargs={'num_samples': 1000, 'batch_size': mod.adata.n_obs, 'use_gpu': True}
+    )
+
+    # Save model
+    run_name = RESULTS_DIR / "cell2location"
+    os.makedirs(run_name, exist_ok=True)
+    mod.save(f"{run_name}", overwrite=True)
+
+    # mod = cell2location.models.Cell2location.load(f"{run_name}", adata_vis)
+
+    # Save anndata object with results
+    adata_file = f"{run_name}/sp.h5ad"
+    adata_vis.write(adata_file)
+
+    mod.plot_QC()
+    plt.savefig(RESULTS_DIR / "QC_spatial_mapping.png")
+    plt.close()
+
+
+def cell2location_xenium(use_gene_intersection: bool = False, cell_types_granularity="cluster"):
 
     # Path to data
     data_path = Path("../../scratch/lbrunsch/data")
@@ -328,69 +391,18 @@ def cell2location_xenium(use_gene_intersection: bool = False, cell_types_granula
     # plot umap as control for cell types granularity
     if cell_types_granularity == "taxonomy":
         plot_umap_ref(annotated_ref_seq, cell_taxonomy=["TaxonomyRank1", "TaxonomyRank2", "TaxonomyRank3", "TaxonomyRank4"])
-
-        # Compute Signature of labelled cell types from single-cell RNA sequencing data
         annotated_ref_seq, inf_aver = signature_ref(annotated_ref_seq, label="TaxonomyRank3")
+
+    elif cell_types_granularity == "cluster":
+        plot_umap_ref(annotated_ref_seq, cell_taxonomy=["ClusterNames"])
+        annotated_ref_seq, inf_aver = signature_ref(annotated_ref_seq, label="ClusterNames")
 
     else:
         raise ValueError("Not implemented yet")
 
-    # Spatial Mapping
+    run_cell2location(annotated_data, inf_aver)
 
-    # find shared genes and subset both anndata and reference signatures
-    intersect = np.intersect1d(annotated_data.var_names, inf_aver.index)
-    adata_vis = annotated_data[:, intersect].copy()
-    inf_aver = inf_aver.loc[intersect, :].copy()
-
-    # prepare anndata for cell2location model
-    cell2location.models.Cell2location.setup_anndata(adata=adata_vis, batch_key="sample")
-
-    # create and train the model
-    mod = cell2location.models.Cell2location(
-        adata_vis, cell_state_df=inf_aver,
-        # the expected average cell abundance: tissue-dependent
-        # hyper-prior which can be estimated from paired histology:
-        N_cells_per_location=1,
-        # hyperparameter controlling normalisation of
-        # within-experiment variation in RNA detection:
-        detection_alpha=20
-    )
-    mod.view_anndata_setup()
-
-    mod.train(max_epochs=30000,
-              # train using full data (batch_size=None)
-              batch_size=None,
-              # use all data points in training because
-              # we need to estimate cell abundance at all locations
-              train_size=1,
-              use_gpu=True,
-              )
-
-    # plot ELBO loss history during training, removing first 100 epochs from the plot
-    mod.plot_history(1000)
-    plt.legend(labels=['full data training'])
-    plt.savefig(RESULTS_DIR / "plot_history_c2l.png")
-    plt.close()
-
-    # In this section, we export the estimated cell abundance (summary of the posterior distribution).
-    adata_vis = mod.export_posterior(
-        adata_vis, sample_kwargs={'num_samples': 1000, 'batch_size': mod.adata.n_obs, 'use_gpu': True}
-    )
-
-    # Save model
-    run_name = RESULTS_DIR / "cell2location"
-    os.makedirs(run_name, exist_ok=True)
-    mod.save(f"{run_name}", overwrite=True)
-
-    # mod = cell2location.models.Cell2location.load(f"{run_name}", adata_vis)
-
-    # Save anndata object with results
-    adata_file = f"{run_name}/sp.h5ad"
-    adata_vis.write(adata_file)
-
-    mod.plot_QC()
-    plt.savefig(RESULTS_DIR / "QC_spatial_mapping.png")
-    plt.close()
+    return 0
 
 
 if "__main__" == __name__:
