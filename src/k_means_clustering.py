5758800c98de07b2350583b99acd5fd5e9edfcbc
LucasBrunschwig <lucas.brunschwig@gmail.com>
2023-11-24 00:04:50 +0100

    Added K means and Leiden Clustering for Spatial Transcriptomics

--- src/k_means_clustering.py
+++ src/k_means_clustering.py
@@ -13,39 +13,39 @@ Description: This file implements a naive approach to match cells with single ce
 import os
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Relative
 from utils import load_xenium_data, load_rna_seq_data
+from visualization import visualize
 
 # Third Party
+
+from sklearn.cluster import KMeans
 import numpy as np
 
 RESULTS_DIR = Path("../../scratch/lbrunsch/results/k_means_clustering")
 os.makedirs(RESULTS_DIR, exist_ok=True)
 
 
-def k_mean_clustering():
-    pass
+def k_mean_clustering(adata, n_clusters, init_centroids):
+    return KMeans(n_clusters=n_clusters, init=init_centroids,
+                  random_state=0, n_init="auto").fit(adata.X.toarray(), n_clusters)
 
 
-def mean_cell_type_expression(adata_ref, label_key):
+def mean_cell_type_expression(adata_ref, label_names, label_key):
 
     print("Computing Mean ")
 
-    label_names = adata_ref.obs[label_key].tolist()
-
     cluster_means = {}
     for label in label_names:
-        cluster_means[label] = np.mean(adata_ref.X[np.where(label_names[0] == adata_ref.obs["ClusterName"])[0], :], axis=0).squeeze(axis=0).A1
+        cluster_means[label] = np.mean(adata_ref.X[np.where(label == adata_ref.obs[label_key])[0], :], axis=0).A1
 
     return pd.DataFrame.from_dict(cluster_means, orient="index")
 
 
-if "__main__" == __name__:
-
-    #
-    compute_cluster_means = False
+def main(label_key_):
 
     # Path to data
     data_path = Path("../../scratch/lbrunsch/data")
@@ -53,22 +53,40 @@ if "__main__" == __name__:
     path_replicate_2 = data_path / "Xenium_V1_FF_Mouse_Brain_MultiSection_2"
     path_replicate_3 = data_path / "Xenium_V1_FF_Mouse_Brain_MultiSection_3"
     paths = [path_replicate_1]
-    path_ref = data_path / "Brain_Atlas_RNA_seq/l5_all.loom"
+    path_ref = data_path / "Brain_Atlas_RNA_seq/l5_all.agg.loom"
 
+    # Load Data
     replicates = []
     for path in paths:
         replicates.append(load_xenium_data(path))
     adata_ref = load_rna_seq_data(path_ref)
 
+    # Match Transcriptomics RNA
     intersect = np.intersect1d(adata_ref.var_names, replicates[0].var_names)
-
     adata_ref = adata_ref[:, intersect]
     for i, replicate in enumerate(replicates):
         replicates[i] = replicate[:, intersect]
 
-    if compute_cluster_means:
-        cluster_means = mean_cell_type_expression(adata_ref, label_key="ClusterName")
-    else:
-        with open(RESULTS_DIR / "cluster_means.pkl", "rb") as file:
-            cluster_means = pd.DataFrame(file)
+    label_names = np.unique(adata_ref.obs[label_key_].tolist())
+    cluster_means = mean_cell_type_expression(adata_ref, label_names, label_key=label_key_).to_numpy()
+
+    for i in range(len(replicates)):
+        k_ = k_mean_clustering(replicates[i], n_clusters=len(adata_ref.obs[label_key_].unique()),
+                               init_centroids=cluster_means)
+
+        # Trying to automatically impute the cell type
+        # labels = [label_names[label_] for label_ in k_.labels_]
+
+        replicates[i].obs["k_means_labels"] = k_.labels_
+        replicates[i].obs["k_means_labels"] = replicates[i].obs["k_means_labels"].astype("category")
+        visualize(replicates[i], "k_means_labels")
+        plt.savefig(RESULTS_DIR / f"{label_key_}_{str(paths[i]).split(os.sep)[-1]}.png")
+        plt.close()
+
+
+if "__main__" == __name__:
+
+    label_key = ["TaxonomyRank1", "TaxonomyRank2", "TaxonomyRank3", "TaxonomyRank4"]
+    for key in label_key:
+        main(key)
 
---
+++ src/leiden_clustering.py
@@ -0,0 +1,119 @@
+"""
+File: leiden_clustering.py
+Author: Lucas Brunschwig
+Email: lucas.brunschwig@hotmail.fr
+GitHub: @LucasBrunschwig
+
+Description: This file implements a naive approach to match cells with single cell RNA seq.
+             The file uses k-means clustering with a number that match the number of cell types
+             and match the centroid to the closest cell types.
+"""
+
+# Std
+import os
+from pathlib import Path
+
+# Third party
+import matplotlib.pyplot as plt
+import scanpy as sc
+import numpy as np
+import seaborn as sns
+
+# Relative
+from utils import load_rna_seq_data, load_xenium_data, preprocess_transcriptomics, get_name_from_path
+from visualization import visualize
+
+# Make directory
+RESULTS_DIR = Path("../../scratch/lbrunsch/results/leiden_clustering")
+os.makedirs(RESULTS_DIR, exist_ok=True)
+RESULTS_DIR_XENIUM = RESULTS_DIR / "xenium"
+os.makedirs(RESULTS_DIR_XENIUM, exist_ok=True)
+RESULTS_DIR_REF = RESULTS_DIR / "ref"
+os.makedirs(RESULTS_DIR_REF, exist_ok=True)
+
+
+def main():
+
+    # Load Data
+    data_path = Path("../../scratch/lbrunsch/data")
+
+    path_ref = data_path / "Brain_Atlas_RNA_seq/l5_all.loom"
+    adata_ref = load_rna_seq_data(path_ref)
+    adata_ref_copy = adata_ref.copy()
+
+    path_replicate_1 = data_path / "Xenium_V1_FF_Mouse_Brain_MultiSection_1"
+    adata_xenium = load_xenium_data(path_replicate_1)
+
+    # Perform Data Preprocessing (intersect data)
+    adata_ref = preprocess_transcriptomics(adata_ref)
+    # intersect = np.intersect1d(adata_ref.var_names, adata_xenium.var_names)
+    # adata_ref = adata_ref[:, intersect]
+
+    adata_xenium.var.index = adata_xenium.var["SYMBOL"]
+    adata_ref.var.index = adata_ref.var["SYMBOL"]
+
+    n_comps = [10, 30, 50]
+    for comp in n_comps:
+        print(f"Adata Ref: {comp}")
+        sc.pp.pca(adata_ref, n_comps=comp)
+        sc.pp.neighbors(adata_ref, n_neighbors=100)  # necessary for UMAP (k-neighbors with weights)
+        sc.tl.umap(adata_ref)
+        sc.tl.leiden(adata_ref)
+        sc.tl.rank_genes_groups(adata_ref, groupby="leiden")
+
+        # # Store the new clusters for C2L
+        # adata_ref_copy.obs["leiden"] = adata_ref.obs["leiden"]
+        # adata_ref_copy.write_loom(RESULTS_DIR / "l5_all_leiden.loom")
+
+        fig1, ax1 = plt.subplots()
+        sc.pl.umap(
+            adata_ref,
+            ax=ax1,
+            color=["leiden"],
+            wspace=0.4,
+            show=False
+        )
+        fig1.savefig(RESULTS_DIR_REF / f"total_brain_mouse_atlas_leiden_PCA{comp}.png", bbox_inches="tight")
+        plt.close(fig1)
+
+        sc.pl.rank_genes_groups(adata_ref, groupby="leiden", show=False)
+        plt.savefig(RESULTS_DIR_REF / f"total_rank_gene_groups_ref_PCA{comp}.png", bbox_inches="tight")
+        plt.close()
+
+    # # Perform data processing
+    # adata_xenium = preprocess_transcriptomics(adata_xenium)
+    #
+    # # Use various principal components to evaluate
+    # n_comps = [10, 20, 30, 40, 50, None]
+    # n_neighbors = 100
+
+    # for n_comp in n_comps:
+    #     print(f"Performing Clustering on Xenium {n_comp}")
+    #     if n_comps is not None:
+    #         sc.pp.pca(adata_xenium, n_comps=n_comp)  # even though not many genes -> noise reduction / batch effect
+    #     sc.pp.neighbors(adata_xenium, n_neighbors=n_neighbors)  # necessary for UMAP (k-neighbors with weights)
+    #     sc.tl.umap(adata_xenium)
+    #     sc.tl.leiden(adata_xenium)
+    #
+    #     sc.tl.rank_genes_groups(adata_xenium, groupby="leiden")
+    #     sc.pl.rank_genes_groups(adata_xenium, show=False)
+    #     plt.savefig(RESULTS_DIR_XENIUM / f"rank_genes_group_{get_name_from_path(path_replicate_1)}_PCA{n_comp}_Neighbors{n_neighbors}.png",
+    #                 bbox_inches="tight")
+    #     plt.close()
+    #
+    #     n_categories = adata_xenium.obs['leiden'].nunique()
+    #     if n_categories > 26:
+    #         palette = sns.color_palette("hls", n_categories)
+    #         adata_xenium.uns['leiden_colors'] = palette
+    #
+    #     visualize(adata_xenium, label_key="leiden")
+    #     plt.savefig(RESULTS_DIR_XENIUM / f"leiden_{get_name_from_path(path_replicate_1)}_PCA{n_comp}_Neighbors{n_neighbors}.png",)
+    #     plt.close()
+
+    return 0
+
+
+if "__main__" == __name__:
+
+    main()
+
