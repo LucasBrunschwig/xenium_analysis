058890206280bd2d954fc9eacb73feee82d2aace
LucasBrunschwig <lucas.brunschwig@gmail.com>
2023-11-24 02:05:37 +0100

    new file: visualization for spatial transcriptomics data

--- src/leiden_clustering.py
+++ src/leiden_clustering.py
@@ -39,20 +39,19 @@ def main():
 
     path_ref = data_path / "Brain_Atlas_RNA_seq/l5_all.loom"
     adata_ref = load_rna_seq_data(path_ref)
-    adata_ref_copy = adata_ref.copy()
 
     path_replicate_1 = data_path / "Xenium_V1_FF_Mouse_Brain_MultiSection_1"
     adata_xenium = load_xenium_data(path_replicate_1)
 
     # Perform Data Preprocessing (intersect data)
     adata_ref = preprocess_transcriptomics(adata_ref)
-    # intersect = np.intersect1d(adata_ref.var_names, adata_xenium.var_names)
-    # adata_ref = adata_ref[:, intersect]
+    intersect = np.intersect1d(adata_ref.var_names, adata_xenium.var_names)
+    adata_ref = adata_ref[:, intersect]
 
     adata_xenium.var.index = adata_xenium.var["SYMBOL"]
     adata_ref.var.index = adata_ref.var["SYMBOL"]
 
-    n_comps = [10, 30, 50]
+    n_comps = [50]
     for comp in n_comps:
         print(f"Adata Ref: {comp}")
         sc.pp.pca(adata_ref, n_comps=comp)
@@ -61,9 +60,10 @@ def main():
         sc.tl.leiden(adata_ref)
         sc.tl.rank_genes_groups(adata_ref, groupby="leiden")
 
-        # # Store the new clusters for C2L
-        # adata_ref_copy.obs["leiden"] = adata_ref.obs["leiden"]
-        # adata_ref_copy.write_loom(RESULTS_DIR / "l5_all_leiden.loom")
+        # Store the new clusters for C2L
+        adata_ref_copy = sc.read_loom(path_ref)
+        adata_ref_copy.obs["leiden"] = adata_ref.obs["leiden"].values.tolist()
+        adata_ref_copy.write_loom(RESULTS_DIR / "l5_all_leiden.loom")
 
         fig1, ax1 = plt.subplots()
         sc.pl.umap(
