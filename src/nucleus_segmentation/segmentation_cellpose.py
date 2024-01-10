79ab3c1eb2e8b58ab04847c1166b0a314ed5893e
LucasBrunschwig <lucas.brunschwig@gmail.com>
2024-01-10 16:42:19 +0100

    test various configuration cellpose

--- src/nucleus_segmentation/segmentation_cellpose.py
+++ src/nucleus_segmentation/segmentation_cellpose.py
@@ -101,7 +101,7 @@ def segment_cellpose(
             # - batch size (224x224 patches to run simultaneously
             # - augment/tile/tile_overlap/resample/interp/cellprob_threshold/min_size/stitch_threshold
 
-            masks, flows, styles, diameters = model.eval(x=[img_], batch_size=4, channels=[0, 0], net_avg=net_avg_,
+            masks, flows, styles, diameters = model.eval(x=[img_], batch_size=2, channels=[0, 0], net_avg=net_avg_,
                                                          diameter=None, do_3D=do_3d_, progress=True, flow_threshold=0.2)
 
     return build_cellpose_mask_outlines(masks)
